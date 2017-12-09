5d9bf5f215f5a89b5e0c60937456c26a34d5e232
Gabriel Ulloa <gsulloa@uc.cl>
2017-12-08 21:55:12 -0300

    Added simple navigation example

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,6 +2,20 @@ import React, { Components, Component } from "react"
 import { StyleSheet, Text, View } from 'react-native'
 import styled from "styled-components/native"
 
+import {
+  Scene,
+  Router,
+  Actions,
+  Reducer,
+  ActionConst,
+  Overlay,
+  Tabs,
+  Modal,
+  Drawer,
+  Stack,
+  Lightbox,
+} from 'react-native-router-flux';
+
 const Body = styled.View`
   flex: 1;
   backgroundColor: #fff;
@@ -9,15 +23,24 @@ const Body = styled.View`
   justify-content: center;
 ` 
 
+const Comp1 = () => <Body>
+<Text>Open up App.js to start working on your app!</Text>
+<Text>Changes you make will automatically reload.</Text>
+<Text>Shake your phone to open the developer menu.</Text>
+<Text>Hello!!!</Text>
+</Body>
+
+const Comp2 = () => <Text>Woho</Text>
+
 class Home extends Component {
   render = () => {
     return (
-      <Body>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Text>Hello!!!</Text>
-      </Body>
+      <Router>
+        <Tabs key="root">
+          <Scene component={Comp1} key="comp1" title="Comp1"/>
+          <Scene component={Comp2} key="comp2" title="Comp2"/>
+        </Tabs>
+      </Router>
     );
   }
 }
