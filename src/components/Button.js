7ac3b51c9ef54f6cc88f48956e68a7c7b5697cb1
Gabriel Ulloa <gsulloa@uc.cl>
2018-01-18 18:03:43 -0300

    Drawer elements

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,3 +3,6 @@ import styled from "styled-components/native"
 export const Button = styled.Button`
   color: ${props => (props.color ? props.color : "#fff")};
 `
+export const TouchableRow = styled.TouchableOpacity`
+  align-items: stretch;
+`
--- src/screens/Drawer.js
+++ src/screens/Drawer.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types"
 import Icon from "react-native-vector-icons/FontAwesome"
 
 import { Screen, NoFlexRow as Row, NoFlexCol as Col } from "@components/View"
+import { TouchableRow } from "@components/Button"
 import { Text } from "@components/Text"
 
 const BorderedRow = Row.extend`
@@ -10,36 +11,56 @@ const BorderedRow = Row.extend`
   border-bottom-width: 1px;
   padding: 15px 0 15px 15px;
   align-items: center;
+  justify-content: flex-start;
 `
 
 const IconContainer = Col.extend`
   padding: 0 20px 0 0;
 `
 
-const ListItem = ({ text, iconName, ...props }) => {
+const ListItem = ({ text, iconName, onPress, ...props }) => {
   return (
-    <BorderedRow {...props}>
-      <IconContainer>
-        <Icon name={iconName} size={20} />
-      </IconContainer>
-      <Col>
-        <Text>{text}</Text>
-      </Col>
-    </BorderedRow>
+    <TouchableRow onPress={onPress}>
+      <BorderedRow {...props}>
+        <IconContainer>
+          <Icon name={iconName} size={20} />
+        </IconContainer>
+        <Col>
+          <Text>{text}</Text>
+        </Col>
+      </BorderedRow>
+    </TouchableRow>
   )
 }
 
 ListItem.propTypes = {
+  onPress: PropTypes.func,
   text: PropTypes.string.isRequired,
   iconName: PropTypes.string.isRequired,
+  underlayColor: PropTypes.string,
 }
 
 class Drawer extends Component {
   state = {
     items: [
-      { id: 0, text: "Image", iconName: "image" },
-      { id: 1, text: "Plane", iconName: "plane" },
-      { id: 2, text: "Info", iconName: "info-circle" },
+      {
+        id: 0,
+        text: "Image",
+        iconName: "image",
+        onPress: () => console.log("press image!"),
+      },
+      {
+        id: 1,
+        text: "Plane",
+        iconName: "plane",
+        onPress: () => console.log("press plane!"),
+      },
+      {
+        id: 2,
+        text: "Info",
+        iconName: "info-circle",
+        onPress: () => console.log("press info!"),
+      },
     ],
   }
 
