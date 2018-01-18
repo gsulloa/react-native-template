9f49962a120562a56203099d58252a625c7dcac1
Gabriel Ulloa <gsulloa@uc.cl>
2018-01-18 16:41:01 -0300

    Added Icons

--- src/Nav.js
+++ src/Nav.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react"
 import { connect } from "react-redux"
+import Icon from "react-native-vector-icons/FontAwesome"
+
 import Home from "@screens/Home"
 import Page from "@screens/Page"
 
@@ -12,8 +14,18 @@ class Nav extends Component {
     return (
       <ConnectedRouter>
         <Tabs key="root">
-          <Scene key="home" component={Home} title="Comp1" />
-          <Scene key="page" component={Page} title="Comp2" />
+          <Scene
+            key="home"
+            component={Home}
+            title="Home"
+            icon={() => <Icon name="home" size={30} />}
+          />
+          <Scene
+            key="page"
+            component={Page}
+            title="Page"
+            icon={() => <Icon name="book" size={30} />}
+          />
         </Tabs>
       </ConnectedRouter>
     )
